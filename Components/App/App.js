caf66bdee0a414f26865f0e7e847be9a22d445de
Anwar Lopez <anwar.lopez@nagarro.com>
2022-06-22 08:25:27 -0500

    initial commit

---
+++ Components/App/App.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+import "./App.css";
+import {Login}  from '../Login';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Login/>
+    </div>
+  );
+};
+
+export default App;
---
+++ Components/Login/Login.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from 'react'
+import './Login.css'
+
+const Login = () => {
+    const [credentials, setCredentials] = useState({
+        user:'',
+        pass:''
+    });
+
+    const verifyAccess = (e) => {
+        e.preventDefault();
+        setCredentials({user:e.target[0].value, pass: e.target[1].value})
+        console.log('e', e.target[0].value)
+        console.log('e', e.target[1].value)
+    }
+
+    const validate = () => {   
+        return true
+    }
+
+    return (
+        <>
+            <div className='login' >Login</div>
+            <form onSubmit={(e)=> {verifyAccess(e)}}>
+                <p>User:</p>
+                <input name='user'/>
+                <p>Password:</p>
+                <input name='pass' type='password'/>
+                <br/>
+                <button type='submit'>
+                    Submit
+                </button>
+            </form>
+
+            <form onSubmit={(e)=> {verifyAccess(e)}}>
+                <p>User:</p>
+                <input name='user'/>
+                <p>Password:</p>
+                <input name='pass' type='password'/>
+                <br/>
+                <button type='submit'>
+                    Submit
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default Login;
